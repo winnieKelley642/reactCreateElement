ff421dc593db8ce7faee89278be6ac88dc644092
Winnie Kelley <alldayallnight642@icloud.com>
2020-05-07 15:45:13 -0400

    only works on safari but not on chrome

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const title = React.createElement(
-    //three arguments
-    //1. type <h1><div> 
-    'h1',
-    //2. object <any attributes "id" null or {} if no attribute
-    //3. contents or children of the elements or null
-    {
-        id:'main-title', title:'This is a title.'
-    },
-    'My First React Element'
-);
-console.log(`this is working`);
-console.log(title);
+function Header() {
+    return (
+      <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+      </header>
+    );
+  }
+  
+  ReactDOM.render(
+    <Header />,
+    document.getElementById('root')
+  );
